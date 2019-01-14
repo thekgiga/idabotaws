ecc614479c38beb8a6eb2d77ad3f12303697f429
thekgiga <thekgiga@gmail.com>
2019-01-14 02:03:28 +0100

    Database connection and implementation

---
+++ src/main/java/db/DatabaseConnection.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+    public Connection conn;
+    private Statement statement;
+    public static DatabaseConnection db;
+
+    private DatabaseConnection() {
+        String url = "jdbc:mysql://idabot.ce5uk1li1gbt.eu-central-1.rds.amazonaws.com:3306/";
+        String driver = "com.mysql.jdbc.Driver";
+        String userName = "thekgiga";
+        String password = "Kragujevac.94";
+        try {
+            Class.forName(driver).newInstance();
+            this.conn = (Connection) DriverManager.getConnection(url, userName, password);
+        } catch (Exception sqle) {
+            sqle.printStackTrace();
+        }
+    }
+
+    /**
+     * @return MysqlConnect Database connection object
+     */
+    public static synchronized DatabaseConnection getInstance() {
+        if (db == null) {
+            db = new DatabaseConnection();
+        }
+        return db;
+
+    }
+
+    /**
+     * @param query String The query to be executed
+     * @return a ResultSet object containing the results or null if not available
+     * @throws SQLException
+     */
+    public ResultSet query(String query) throws SQLException {
+        statement = db.conn.createStatement();
+        ResultSet res = statement.executeQuery(query);
+        return res;
+    }
+
+    /**
+     * @param insertQuery String The Insert query
+     * @return boolean
+     * @throws SQLException
+     * @desc Method to insert data to a table
+     */
+    public int insert(String insertQuery) throws SQLException {
+        statement = db.conn.createStatement();
+        int result = statement.executeUpdate(insertQuery);
+        return result;
+
+    }
+}
--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,14 +1,36 @@
 package hello;
 
+import db.DatabaseConnection;
+import org.json.JSONArray;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+
+        ArrayList result = new ArrayList<>();
+        result.add("---GREETINGS---");
+        try {
+            ResultSet rs = DatabaseConnection.getInstance().query("SELECT * FROM ida.room;");
+            while (rs.next()) {
+                String instruction = rs.getString("instructions");
+                String idroom = rs.getString("idroom");
+                String floor = rs.getString("floor");
+                result.add("INSTRUCTION :" + instruction + " for room " + idroom + " on floor " + floor + "\t");
+            }
+
+        } catch (SQLException e) {
+            System.out.println("exception shit happened");
+        }
+
+        return new JSONArray(result).toString();
     }
 
 }
